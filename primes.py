c1d1575d3d7a95c30555e10390dbbf3921039dd3
Ryan Bunting <ryan.bunting@kcl.ac.uk>
2022-10-09 18:14:17 +0100

    Solve challenge

--- primes.py
+++ primes.py
@@ -3,4 +3,23 @@
 
 def primes(number_of_primes):
     list = []
+    i = 2
+
+    if (number_of_primes < 1):
+        raise (ValueError)
+
+    while(1):
+        if(isPrime(i) == True):
+            list.append(i)
+            if(len(list) == number_of_primes):
+                break
+            i += 1
+        else:
+            i+=1
     return list
+
+def isPrime(n):
+    for i in range(2, n//2 + 1):
+        if(n % i == 0):
+            return False
+    return True
